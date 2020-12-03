9bb3cb835fba2eb5d985ba355b7939ec6465a649
john <sakthichunkz17@gmail.com>
2020-12-03 05:11:52 -0800

    Allowed Host

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^b0d4&x2p*g00dk1rzb$j2tu*srs+!sl$(mza0yrj+m((z9ube'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1' , '[.pythonanywhere.com]']
 
