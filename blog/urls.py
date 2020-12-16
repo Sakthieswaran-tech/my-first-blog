7860bf5b2ccde073b16c52c598553a9ea4efec1c
john <sakthichunkz17@gmail.com>
2020-12-16 01:55:04 -0800

    added all

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 urlpatterns = [
 	path('',views.post_list,name='post_list'),
-	]
+	path('add',views.add,name='add')]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.http import HttpResponse
 def post_list(request):
-	return render(request,'blog/post_list.html',{})
+   return render(request,'blog/post_list.html',{'name':'Sakthi'})
+def add(request):
+      # if request.method=='post':
+      #    form = ContactForm(request.POST)
+      #    if form.is_valid():
+   val1 = int(request.POST['num1'])
+   val2 = int(request.POST['num2'])
+   res = val1+val2
+   return render(request,'blog/result.html',{'result':res})
+
 # Create your views here.
