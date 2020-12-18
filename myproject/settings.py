0173d358d6ae0b4cc5404231c44e8b29ee6b08cd
john <sakthichunkz17@gmail.com>
2020-12-18 08:06:10 -0800

    added static

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'travello.apps.TravelloConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'myproject/static')
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
